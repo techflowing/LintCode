b19e17b25a4f03980e6748ab20dddf013bb8d943
techflowing <techflowing@gmail.com>
2018-02-09 17:48:53 +0800

    【036】

--- src/M_036_ReverseLinkedListII.java
+++ src/M_036_ReverseLinkedListII.java
@@ -4,7 +4,7 @@ import model.ListNode;
  * 翻转链表中第m个节点到第n个节点的部分
  * 给出链表1->2->3->4->5->null， m = 2 和n = 4，返回1->4->3->2->5->null
  */
-public class E_036_ReverseLinkedListII {
+public class M_036_ReverseLinkedListII {
 
     /**
      * 首先创建一个tempHead指向头结点，并使用他来定位到第m个节点之前的节点，将curNode设置为preNode之后的节点，
